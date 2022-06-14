3e152b487ae0941637bf8836597d6f4360c29a0f
Eli Whynot <eliwhynot@Elis-MacBook-Pro.local>
2022-06-13 22:02:34 -0300

    work on form submit

--- formSubmit.js
+++ formSubmit.js
@@ -1,6 +1,11 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
     'use strict'
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+    var theUrl = "http://localhost:8081/form";
+    xmlhttp.open("POST", theUrl);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
     document.getElementById('rsvp-form').onsubmit = (event)=>{
         const formValues = Object.entries(event.target).map((pair) =>{
             const {type, name, value, checked} = pair[1];
@@ -10,6 +15,7 @@
             console.log(formName, formValue)
             return {formName: formValue}
         })
+        xmlhttp.send(JSON.stringify({ "email": "hello@user.com", "response": { "name": "Tester" } }));
         console.log(formValues)
     }
 })()
