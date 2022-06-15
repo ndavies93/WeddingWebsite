aa1116ddd403e72733d7b0b471241110e8c5da12
Eli Whynot <eliwhynot@Elis-MacBook-Pro.local>
2022-06-15 20:41:15 -0300

    change stuff

--- formSubmit.js
+++ formSubmit.js
@@ -1,21 +1,24 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
     'use strict'
-    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
-    var theUrl = "http://localhost:8081/form";
-    xmlhttp.open("POST", theUrl);
+    const xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+    const theUrl = "https://whynot-get-married-backend.herokuapp.com/form";
+    xmlhttp.open("POST", theUrl, true);
     xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
     document.getElementById('rsvp-form').onsubmit = (event)=>{
         const formValues = Object.entries(event.target).map((pair) =>{
             const {type, name, value, checked} = pair[1];
             const formName = type === 'radio' ? value : name;
-
+            console.log(formName);
             const formValue = type === 'radio' ? checked : value;
-            console.log(formName, formValue)
-            return {formName: formValue}
+            return {[formName]: formValue}
         })
-        xmlhttp.send(JSON.stringify({ "email": "hello@user.com", "response": { "name": "Tester" } }));
-        console.log(formValues)
+        xmlhttp.onreadystatechange = function() { // Call a function when the state changes.
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                console.log(this.responseText);
+            }
+        }
+        xmlhttp.send(JSON.stringify(formValues));
     }
 })()
