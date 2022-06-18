022cbe6efd8540be02b9a205bdaa141dadb439a8
Eli Whynot <eliwhynot@Elis-MacBook-Pro.local>
2022-06-18 10:13:02 -0300

    actually finish form

--- formSubmit.js
+++ formSubmit.js
@@ -1,9 +1,7 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
     'use strict'
-    const xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+    const xmlhttp = new XMLHttpRequest();
     const theUrl = "https://whynot-get-married-backend.herokuapp.com/form";
-    // const theUrl = "http://localhost:8081/form";
     xmlhttp.open("POST", theUrl, true);
     xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
@@ -11,13 +9,12 @@
         event.preventDefault();
         const formValues = Object.entries(event.target).map((pair) =>{
             const {type, name, value, checked} = pair[1];
-            const formName = type === 'radio' ? value : name;
-            const formValue = type === 'radio' ? checked : value;
+            const formName = type === 'radio' || type === 'checkbox' ? value : name;
+            const formValue = type === 'radio' || type === 'checkbox'? checked : value;
             return {[formName]: formValue}
         })
         xmlhttp.onreadystatechange = function() { // Call a function when the state changes.
             if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-                console.log(this.responseText);
             } else {
                 console.log('error',this.status, this.statusText)
             }
